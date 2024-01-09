dc1174791576bde06bc5d946355b89cc4651065e
Cristiano Bennertz <cristiano.bennertz@gmail.com>
2024-01-09 01:41:30 -0300

    Melhoria de codificação.

--- app/src/main/java/sp/senai/MainActivity.java
+++ app/src/main/java/sp/senai/MainActivity.java
@@ -1,15 +1,12 @@
 package sp.senai;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,16 +14,14 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    // ListView
-    private ListView lvEstudantes;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Referência da lista a partir da resource id
-        lvEstudantes = findViewById(R.id.lvEstudantes);
+        // ListView
+        ListView lvEstudantes = findViewById(R.id.lvEstudantes);
 
         // Criação de lista de estudantes
         String[] nomes = {"Estudante 1", "Estudante 2", "Estudante 3", "Estudante 4", "Estudante 5", "Estudante 6"};
@@ -38,38 +33,27 @@ public class MainActivity extends AppCompatActivity {
         // Indicar os dados por trás da ListView
         lvEstudantes.setAdapter(adapter);
 
-        lvEstudantes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Diálogo de alerta
-                AlertDialog alerta;
-                // Estudante selecionado da lista
-                String estudanteSelecionado = (String) parent.getAdapter().getItem(position);
-                //Construtor do alerta
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                // Título
-                builder.setTitle("Atenção");
-                // Mensagem
-                builder.setMessage("Você quer deletar o registro?");
-                // Botão Sim
-                builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface arg0, int arg1) {
-                        estudantes.remove(estudanteSelecionado);
-                        adapter.notifyDataSetChanged();
-                    }
-                });
-                // Botão Não
-                builder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(MainActivity.this, "O estudante " + estudanteSelecionado + " não foi deletado =)", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                alerta = builder.create();
-                // Apresentar na tela
-                alerta.show();
-            }
+        lvEstudantes.setOnItemClickListener((parent, view, position, id) -> {
+            // Diálogo de alerta
+            AlertDialog alerta;
+            // Estudante selecionado da lista
+            String estudanteSelecionado = (String) parent.getAdapter().getItem(position);
+            //Construtor do alerta
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            // Título
+            builder.setTitle("Atenção");
+            // Mensagem
+            builder.setMessage("Você quer deletar o registro?");
+            // Botão Sim
+            builder.setPositiveButton("Sim", (arg0, arg1) -> {
+                estudantes.remove(estudanteSelecionado);
+                adapter.notifyDataSetChanged();
+            });
+            // Botão Não
+            builder.setNegativeButton("Não", (dialog, which) -> Toast.makeText(MainActivity.this, "O estudante " + estudanteSelecionado + " não foi deletado =)", Toast.LENGTH_SHORT).show());
+            alerta = builder.create();
+            // Apresentar na tela
+            alerta.show();
         });
     }
 }
